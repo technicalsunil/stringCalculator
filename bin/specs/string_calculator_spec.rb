972120169ff095b5d179be24e234a22d22dec7ed
Sunil Kumar <technicalsunil@outlook.com>
2024-03-05 22:45:09 +0530

    return 0 if string empty

--- bin/specs/string_calculator_spec.rb
+++ bin/specs/string_calculator_spec.rb
@@ -1,4 +1,10 @@
 require_relative '../../lib/string_calculator'
 
 describe StringCalculator do
+	let(:scal) { scal = StringCalculator.new }
+	describe "#add" do
+		it "return 0 if string empty" do
+			expect(scal.add("")).to eq(0)
+		end
+	end
 end
--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,2 +1,5 @@
 class StringCalculator
+	def add(value)
+		return 0 if value.empty?
+	end
 end
